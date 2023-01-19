a04eb9576baf838618939a2e9e18806d637a6630
oussama rouabah <rouabaho@gmail.com>
2023-01-19 12:04:41 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/oussamarouabah/use-arithmetic
+
+go 1.19
+
+require github.com/oussamarouabah/arithmetic v0.0.0-20230119110229-219cb1241e3c
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/oussamarouabah/arithmetic"
+)
+
+func main() {
+	fmt.Println(arithmetic.Sum(1, 2, 3, 4, 5, 6, 7, 8, 9))
+}
